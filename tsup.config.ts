918d1b7e5656f8bb09e4a0d83e1c788310cf22c5
Augusto <aspastrana990@gmail.com>
2025-01-22 00:23:09 -0300

    Exlucde sourcemaps

--- tsup.config.ts
+++ tsup.config.ts
@@ -4,8 +4,9 @@ export default defineConfig({
   entry: ["src/index.ts"], // Archivo de entrada
   format: ["esm", "cjs"], // Formatos: ESModules y CommonJS
   dts: true, // Genera archivos de declaración (.d.ts)
-  sourcemap: true, // Incluye mapas de fuente
+  sourcemap: false, // Incluye mapas de fuente
   clean: true, // Limpia la carpeta "dist" antes de empaquetar
   splitting: true, // Habilita el "splitting" de dependencias
   treeshake: true, // Elimina código no usado
+  minify: true, // Minificar archivos
 });
